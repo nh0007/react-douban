ea57d809fb4e3573a7d78030cc33d7cb63fb8e3e
nh0007 <nh0828007@gmail.com>
2018-06-10 13:58:45 +0800

    添加scss依赖配置

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -157,7 +157,7 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.css$/,
+            test: /\.(css|scss)$/,
             use: [
               require.resolve('style-loader'),
               {
@@ -186,6 +186,9 @@ module.exports = {
                   ],
                 },
               },
+              {
+                loader: require.resolve('sass-loader')
+              }
             ],
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
