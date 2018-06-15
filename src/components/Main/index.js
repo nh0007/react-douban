fdd8da0aee7de4fcbd31463687d87c60e8bdb9d5
nh0007 <nh0828007@gmail.com>
2018-06-15 23:08:01 +0800

    modify router config

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import BookTagContent from '../Book/BookTagContent';
 import styles from './main.scss';
 
 export default function Main() {
   return (
     <main className={styles['main-content']}>
-      <BookTagContent />
+      <Switch>
+        <Route exact path="/" component={BookTagContent} />
+        <Route exact path="/book-tag" component={BookTagContent} />
+      </Switch>
     </main>
   );
 }
